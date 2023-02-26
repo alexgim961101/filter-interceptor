91617ffb1bfa6ae91030fb4f6d9f796c6fb02de8
alexgim <alexgim961101@gmail.com>
2023-02-26 13:09:18 +0900

    first commit

---
+++ src/test/java/com/alexgim/filterinterceptor/FilterInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexgim.filterinterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
